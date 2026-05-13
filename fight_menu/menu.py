1477610b09aacd91f64bf90415f9d4283e5ea9af
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-13 10:23:04 +0800

    created the basic fighting menu

---
+++ fight_menu/menu.py
@@ -0,0 +1,39 @@
+from calculations.math import Calculations
+
+class FightMenu:
+    def __init__(self, player, enemy):
+        self.my_character = player
+        self.current_enemy = enemy
+
+    def display_menu(self):
+        while True:
+            print(f"""
+            {self.my_character.__class__.__name__}              VS          {self.current_enemy.__class__.__name__}
+            hp={self.my_character.hp}                                        hp={self.current_enemy.hp}""")
+            players_choice = input(f"""
+            1. Attack""")
+            if players_choice == "1":
+                calc = Calculations()
+                if calc.accuracy(self.current_enemy.evasion):
+                    dmg_dealt = calc.damage(self.my_character.attack_power, self.current_enemy.defense)
+                    self.current_enemy.hp -= dmg_dealt
+                    print(f"Attack Hit, {dmg_dealt} dmg")
+
+                else:
+                    print(f"Miss")
+
+                if self.current_enemy.hp <= 0:
+                    print(f"The {self.current_enemy.__class__.__name__} is dead")
+                    break
+
+                print(f" The {self.current_enemy.__class__.__name__} attacks")
+                if calc.accuracy(self.my_character.evasion):
+                    enemy_dmg = calc.damage(self.current_enemy.attack, self.my_character.defense)
+                    self.my_character.hp -= enemy_dmg
+                    print(f"You took {enemy_dmg} damage")
+                else:
+                    print("You dodged")
+
+                if self.my_character.hp <= 0:
+                    print(f"Game Over")
+                    break
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from calculations.math import Calculations
 from character.pick_character import CharacterSelection
 from main_menu.menu import Start
 from enemies.enemy_spawner import EnemySpawner
+from fight_menu.menu import FightMenu
 
 my_game = Start("start")
 my_game.run_menu()
@@ -14,12 +15,6 @@ current_enemy = spawner.spawn_random_enemies()
 
 print(f"A {current_enemy.__class__.__name__} appeared")
 
-# calc = Calculations()
-# if calc.accuracy(current_enemy.evasion):
-#     dmg_dealt = calc.damage(my_character.attack_power, current_enemy.defense)
-#     current_enemy.hp -= dmg_dealt
-#     print (f"Attack Hit, {dmg_dealt} dmg")
-# else:
-#     print (f"Miss")
-#
+battle_screen = FightMenu(my_character, current_enemy)
+battle_screen.display_menu()
 
